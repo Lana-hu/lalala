54c61a1de90aaa7ad94b18217fcebef113f93a59
Lana-hu <94089214+Lana-hu@users.noreply.github.com>
2021-12-06 09:26:23 -0300

    Add files via upload

---
+++ login.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang= "pt-br">
+<head>
+    <meta charset= "UTD-8">
+    <meta http-equiv= "X-UA-Compatible" content= "IE=edge">
+    <meta name= "viewpot" content= "width=device-width, initial-scale">
+    <title>login Cliente</title>
+</head>
+<body>
+    <div class="caixa">
+        <form action="index.php" method= "get">
+            <label for= "login"> Login</label>
+            <imput type="text" name= "login">
+            <label for= "senha">Senha</label>
+            <imput type= "password" name="senha">
+
+        <imput type= "submit" value="Entrar" name="en">
+</form>
+</div>
+</body>
+</html>
+    
